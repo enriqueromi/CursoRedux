94a1e2dd6529db1c5dd3f9501669f17d32823d31
enriquerojas <enrique.rojas@s-dos.es>
2020-12-15 11:51:23 +0100

    seccion 12

--- 05-http-effects/src/app/usuarios/lista/lista.component.ts
+++ 05-http-effects/src/app/usuarios/lista/lista.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../models/usuario.model';
+import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.scss']
+  styleUrls: ['./lista.component.scss'],
 })
 export class ListaComponent implements OnInit {
+  usuarios: Usuario[] = [];
 
-  constructor() { }
+  constructor(public usuariosService: UsuariosService) {}
 
   ngOnInit(): void {
+    this.usuariosService.getUser().subscribe((users) => {
+      this.usuarios = users;
+    });
   }
-
 }
